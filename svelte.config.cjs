ecefb3fe541c0c7a348572edc6e198adfca075f1
WormCrew <44246461+WormCrew@users.noreply.github.com>
2021-03-30 01:55:57 +0100

    update

--- svelte.config.cjs
+++ svelte.config.cjs
@@ -1,5 +1,5 @@
 const sveltePreprocess = require('svelte-preprocess');
-const node = require('@sveltejs/adapter-netlify');
+const node = require('@sveltejs/adapter-static');
 const pkg = require('./package.json');
 
 /** @type {import('@sveltejs/kit').Config} */
