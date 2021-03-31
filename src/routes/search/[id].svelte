9ed3ae2058d85dc5e036101cb8a702f91e8bd72e
WormCrew <44246461+WormCrew@users.noreply.github.com>
2021-03-31 23:18:23 +0100

    Update [id].svelte
    amended search api

--- src/routes/search/[id].svelte
+++ src/routes/search/[id].svelte
@@ -4,8 +4,8 @@
 	import { media_type } from '$lib/store'
 	import Spinner from '$lib/Spinner.svelte';
 
-  const ApiKey = process.env.SAPPER_APP_API_KEY
-	const SEARCH_API = `https://api.themoviedb.org/3/search/${$media_type}?api_key=${ApiKey}&language=en-GB"&page=1&include_adult=true&query="`
+  	const ApiKey = process.env.SAPPER_APP_API_KEY
+	const SEARCH_API = `https://api.themoviedb.org/3/search/${$media_type}?api_key=${ApiKey}&language=en-GB"&page=1&include_adult=true&query=`
 	
 	$: api_url = (SEARCH_API + $page.params.id + '&page=')
 
