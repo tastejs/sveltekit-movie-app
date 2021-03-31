0f73e7fbcfc9f2eebd29678684079809db5f656b
WormCrew <44246461+WormCrew@users.noreply.github.com>
2021-03-31 23:42:59 +0100

    search route updated
    api-key referenced from store.ts

--- src/routes/search/[id].svelte
+++ src/routes/search/[id].svelte
@@ -1,11 +1,10 @@
 <script>
 	import MainSection from '$lib/MainSection.svelte'
 	import { page } from '$app/stores';
-	import { media_type } from '$lib/store'
+	import { media_type, ApiKey } from '$lib/store'
 	import Spinner from '$lib/Spinner.svelte';
 
-  	const ApiKey = process.env.SAPPER_APP_API_KEY
-	const SEARCH_API = `https://api.themoviedb.org/3/search/${$media_type}?api_key=${ApiKey}&language=en-GB"&page=1&include_adult=true&query=`
+	const SEARCH_API = `https://api.themoviedb.org/3/search/${$media_type}?api_key=${$ApiKey}&language=en-GB"&page=1&include_adult=true&query=`
 	
 	$: api_url = (SEARCH_API + $page.params.id + '&page=')
 
